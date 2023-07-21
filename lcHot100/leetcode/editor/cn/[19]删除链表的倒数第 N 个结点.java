b25912336654fd11d398a590e2ee6f4027712fa8
yang.songlin <yang.songlin@tslsmart.com>
2023-07-21 18:05:11 +0800

    链表删除倒数第K个点

---
+++ lcHot100/leetcode/editor/cn/[19]删除链表的倒数第 N 个结点.java
@@ -0,0 +1,77 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// Related Topics 链表 双指针 👍 2593 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/**
+ * 双指针之快慢指针 --- 快指针先走n步
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode l, int n) {
+        ListNode head = new ListNode();
+        head.next = l;
+        ListNode fast = head;
+        ListNode low = head;
+        while (n -- > 0 ) fast = fast.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            low = low.next;
+        }
+        low.next = low.next.next;
+        return head.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
